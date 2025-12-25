546e06fbe0c31cb9b59d85574ef686c829d16667
Nikhil <ektak144@gmail.com>
2025-12-25 18:41:42 +0530

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import React, { useContext } from 'react'
-import Nav from './components/navbar/Nav'
-import BottomBar from './components/bottomBar/BottomBar'
+import React, { useContext } from "react";
+import Nav from "./components/navbar/Nav";
+import Contact from "./components/ContactPage/Contact";
+import BottomBar from "./components/bottomBar/BottomBar";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  
   return (
-    <div className='main'>
+    <div className="main">
       <Nav />
       {/* <Contact /> */}
       {/* <BottomBar /> */}
-    </div>
-  )
-}
 
-export default App
+      <Routes>
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
