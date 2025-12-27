a6d9e9a00a16326fa2074defa4b77ef4e797a746
Manish2702-I <manish7056540236@gmail.com>
2025-12-27 14:11:35 +0530

    form-route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React, { useContext } from 'react'
-import Contact from './components/ContactPage/Contact'
-import Nav from './components/navbar/Nav'
-import BottomBar from './components/bottomBar/BottomBar'
+import React, { useContext } from "react";
+import Contact from "./components/ContactPage/Contact";
+import Nav from "./components/navbar/Nav";
+import BottomBar from "./components/bottomBar/BottomBar";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="main">
-     <Nav />
-      {/* <Contact /> */}
-      {/* <BottomBar /> */}
+      <Nav />
+      <BottomBar />
 
       <Routes>
         <Route path="/contact" element={<Contact />} />
