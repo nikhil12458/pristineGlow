7e19d84d9806671446d3aad4a6386685f0ec02dd
Nikhil <ektak144@gmail.com>
2025-12-30 02:13:00 +0530

    adding more root variables

--- src/components/burn/Burn.jsx
+++ src/components/burn/Burn.jsx
@@ -1,19 +1,24 @@
 import { useContext } from "react";
 import BurnCard from "../card/BurnCard";
 import { CandleContext } from "../../context/CandleProvider";
+import { Helmet } from "react-helmet-async";
 
 const Burn = () => {
   const { burnData } = useContext(CandleContext);
-  console.log(burnData)
   return (
     <div className="burnBody">
+      <Helmet>
+        <title>Glow | Pristine Glow</title>
+        <meta
+          name="description"
+          content="Discover essential candle care tips to enhance your candle experience. Learn how to burn candles safely and effectively with our expert advice."
+        />
+      </Helmet>
       <h1 className="glowHeading">✨ Glow – Candle Care Tips</h1>
       <div className="glowCards">
-        {
-        burnData.map((item, idx) => {
-          return <BurnCard key={idx} item={item} />
-        })
-      }
+        {burnData.map((item, idx) => {
+          return <BurnCard key={idx} item={item} />;
+        })}
       </div>
     </div>
   );
