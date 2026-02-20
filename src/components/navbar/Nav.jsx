0af9584e1aebaebf85821f1b18b45e6c1fcab1d6
Nikhil <ektak144@gmail.com>
2026-02-20 23:40:26 +0530

    adding top logo and sorting using category

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -6,33 +6,38 @@ const Nav = () => {
   const navElem = ["Home", "Contact", "Custom-Order", "Glow"];
 
   return (
-    <motion.nav
-      initial={{
-        y: "-50%",
-        opacity: 0,
-      }}
-      whileInView={{
-        y: "0%",
-        opacity: 1,
-      }}
-      transition={{
-        duration: 0.6,
-        ease: easeIn,
-      }}
-      className="navbar"
-    >
-      {navElem.map((item, idx) => {
-        return (
-          <Link
-            className="nav-items"
-            key={idx}
-            to={item === "Home" ? "/" : `/${item.toLowerCase()}`}
-          >
-            {item}
-          </Link>
-        );
-      })}
-    </motion.nav>
+    <div className="logoSection">
+      <Link to={"/"} className="nav-side-logo">
+        <img src="/logo/mainLogo.svg" alt="" />
+      </Link>
+      <motion.nav
+        initial={{
+          y: "-50%",
+          opacity: 0,
+        }}
+        whileInView={{
+          y: "0%",
+          opacity: 1,
+        }}
+        transition={{
+          duration: 0.6,
+          ease: easeIn,
+        }}
+        className="navbar"
+      >
+        {navElem.map((item, idx) => {
+          return (
+            <Link
+              className="nav-items"
+              key={idx}
+              to={item === "Home" ? "/" : `/${item.toLowerCase()}`}
+            >
+              {item}
+            </Link>
+          );
+        })}
+      </motion.nav>
+    </div>
   );
 };
 
