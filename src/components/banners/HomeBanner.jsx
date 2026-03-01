7a230c7a31480665492a3af7c88a9af77a06cf33
Nikhil <ektak144@gmail.com>
2026-03-01 20:24:48 +0530

    Revert "Improve SEO metadata and client-side performance"

--- src/components/banners/HomeBanner.jsx
+++ src/components/banners/HomeBanner.jsx
@@ -1,24 +1,15 @@
-import React from "react";
+import React from 'react'
 
 const HomeBanner = () => {
   return (
     <section className="homeBanner">
-      <img
-        src="/banner/homeBanner.webp"
-        alt="Decorative candle arrangement banner"
-        loading="eager"
-        fetchPriority="high"
-        decoding="async"
-      />
-      <div className="bannerContent">
-        <img src="/logo/mainLogo.svg" alt="Pristine Glow logo" loading="eager" />
-        <h4>
-          Pristine <br />
-          <span>Glow</span>
-        </h4>
-      </div>
+        <img src="/banner/homeBanner.webp" alt="" />
+        <div className="bannerContent">
+          <img src="/logo/mainLogo.svg" alt="" />
+          <h4>Pristine <br /><span>Glow</span></h4>
+        </div>
     </section>
-  );
-};
+  )
+}
 
-export default HomeBanner;
+export default HomeBanner
